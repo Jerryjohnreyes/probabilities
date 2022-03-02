c42d841617309e143d0f1ac84c8d7fa4ca8a0a96
Jerry Reyes <jerryjohnreyes@gmail.com>
2022-03-01 20:08:59 -0800

    Exercise 002 made with R software. Simulation of the probability that a five-card poker hand has exactly two kings.

--- exercise002.R
+++ exercise002.R
@@ -1,14 +1,27 @@
 exercise002 <- function(){
+  'This function will return the number of kings that a five-card poker hand has.'
   poker_hand = vector(mode='list', length = 5)
   for (i in c(1:5)){
     poker_hand[i] = round(runif(1,1,52))
   }
-  print(poker_hand)
   kings_in_hand = vector(mode='list')
   for (i in c(1:5)){
-    if (poker_hand[i] %% 13){
+    if (poker_hand[[i]] %% 13 == 0){
       kings_in_hand[i] = 1
     }else{kings_in_hand[i]=0}
   }
-  print(kings_in_hand)
+  #print(kings_in_hand)
+  return(sum(unlist(kings_in_hand)))
+}
+
+probability002 <- function(number){
+  'This function will return the approximate probability that a poker hand'
+  'has exactly two kings.'
+  ocurrences = vector(mode='list', length = number)
+  for (i in c(1:number)){
+    number_of_kings = exercise002()
+    if(number_of_kings == 2){ocurrences[[i]]=1}
+    else{ocurrences[[i]]=0}
+  }
+  return(sum(unlist(ocurrences))/number)
 }
